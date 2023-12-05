cd47cc4c4a96018c548f413833c204b7583756cb
farasat <farasatullahk@gmail.com>
2023-12-05 16:01:29 +0500

    Initial Commit

---
+++ BackgroundRemovalApp/BackgroundRemovalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundRemovalAppApp.swift
+//  BackgroundRemovalApp
+//
+//  Created by Farasat_Niazi on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundRemovalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
